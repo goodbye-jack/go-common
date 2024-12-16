6019843c0313270880924ef9e332bbb591343b27
wangzl8 <wangzl8@asiainfo.com>
2024-12-16 21:56:34 +0800

    @20241216@Santy@增加内置子查询方法的分页

--- orm/orm.go
+++ orm/orm.go
@@ -5,6 +5,7 @@ import (
 	"github.com/goodbye-jack/go-common/log"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 type Orm struct {
@@ -13,7 +14,8 @@ type Orm struct {
 
 func NewOrm(dsn string) *Orm {
 	log.Info("NewOrm param:dsn=", dsn)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// &gorm.Config{Logger: logger.Default.LogMode(logger.Info)}
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
 	if err != nil {
 		log.Fatal("mysql connect failed, %v", err)
 	}
