43be3d7892f6cda710a1fa620088189a3b825acb
Santy5581 <Santy0921@163.com>
2025-05-14 18:41:06 +0800

    @20250514@Santy@修改暖城的数据审批流程接口-2

--- http/utils.go
+++ http/utils.go
@@ -55,17 +55,54 @@ func JsonResponse(c *gin.Context, data interface{}, err error) {
 	})
 }
 
-func ResponseSuccess(c *gin.Context, data interface{}, msg string, err error) {
+// 业务异常结构体
+type BusinessError struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+func (e *BusinessError) Error() string {
+	return e.Message
+}
+
+// 参数异常结构体
+type ParameterError struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+// 系统异常
+type SystemError struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+func (e *ParameterError) Error() string {
+	return e.Message
+}
+
+func JsonResponseNew(c *gin.Context, data interface{}, err error) {
 	statusCode := 200
-	message := "success"
+	var responseMessage string
 	if err != nil {
 		data = nil
-		message = whichError(err)
-		statusCode = 500
+		switch err := err.(type) {
+		case *BusinessError:
+			statusCode = err.Code
+			responseMessage = err.Message
+		case *ParameterError:
+			statusCode = err.Code
+			responseMessage = err.Message
+		default: //
+			statusCode = 500
+			responseMessage = whichError(err)
+		}
+	} else {
+		responseMessage = "success"
 	}
 	c.JSON(statusCode, gin.H{
 		"data":    data,
-		"message": message,
+		"message": responseMessage,
 	})
 }
 
