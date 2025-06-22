4ba013efae444fc8a19ce349a49ef08fca1e8d8f
Santy5581 <Santy0921@163.com>
2025-06-22 21:27:28 +0800

    @20250622@Santy@修改暖城的数据审批流程接口-22-3

--- http/utils.go
+++ http/utils.go
@@ -39,14 +39,14 @@ func AddTokenCookie(c *gin.Context, token string, tokenExpired int) {
 	c.SetCookie(tokenName, token, tokenExpired, "/", "", false, true)
 }
 
-func SetTokenCookie(c *gin.Context, token string, tokenExpired int, secure, httpOnly bool) {
+func SetTokenCookie(c *gin.Context, token string, tokenExpired int, domain string, secure, httpOnly bool) {
 	tokenName := config.GetConfigString(utils.ConfigNameToken)
 	if tokenName == "" {
 		log.Warn("!!!!!!!!!!!token name is empty!!!!!!!")
 		tokenName = "good_token"
 	}
 	log.Info("token name = %s", tokenName)
-	c.SetCookie(tokenName, token, tokenExpired, "/", "", secure, httpOnly)
+	c.SetCookie(tokenName, token, tokenExpired, "/", domain, secure, httpOnly)
 }
 
 func JsonResponse(c *gin.Context, data interface{}, err error) {
